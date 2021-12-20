fdec5782d10adceb03455caea265be23019f720e
Kasra Keshvardoost <kasrakeshvardoost@gmail.com>
2021-12-20 20:11:19 +0330

    courses api

--- src/controllers/courses.controller.ts
+++ src/controllers/courses.controller.ts
@@ -72,4 +72,95 @@ export class CoursesController {
         // return res.json(modifiedTeachers);
         return res.json(teachers);
     }
+
+    @Get("/courses")
+    async getCourses(@Req() req: Request, @Res() res: Response): Promise<void | Response> {
+        const page = req.query.page ? parseInt(req.query.page.toString()) : 1;
+        const pp = req.query.pp ? parseInt(req.query.pp.toString()) : 10;
+
+        const search = req.query.search ? req.query.search.toString() : "";
+        const order = req.query.order ? req.query.order.toString() : "";
+        const group = req.query.group ? req.query.group.toString() : "";
+
+        // the base query object
+        let query = {
+            status: "active",
+        };
+        if (!!group) {
+            query["groups.topGroup"] = group;
+        }
+
+        // sort
+        let sort = {};
+        switch (order) {
+            case "most-popular":
+                sort["buyCount"] = "desc";
+                break;
+            case "oldest":
+                sort["publishedAt"] = "asc";
+                break;
+            default:
+                sort["publishedAt"] = "desc";
+        }
+
+        // making the model with query
+        let data = this.CourseModel.aggregate();
+        data.lookup({
+            from: "coursegroups",
+            localField: "groups",
+            foreignField: "_id",
+            as: "groups",
+        });
+        data.lookup({
+            from: "users",
+            localField: "teacher",
+            foreignField: "_id",
+            as: "teacher",
+        });
+        data.match(query);
+        data.match({
+            $or: [
+                { name: { $regex: new RegExp(`.*${search}.*`, "i") } },
+                { description: { $regex: new RegExp(`.*${search}.*`, "i") } },
+                { "teacher.name": { $regex: new RegExp(`.*${search}.*`, "i") } },
+                { "teacher.family": { $regex: new RegExp(`.*${search}.*`, "i") } },
+                { "groups.name": { $regex: new RegExp(`.*${search}.*`, "i") } },
+                { tags: { $regex: new RegExp(`.*${search}.*`, "i") } },
+                { "topics.name": { $regex: new RegExp(`.*${search}.*`, "i") } },
+            ],
+        });
+        data.sort(sort);
+        data.project("teacher.image teacher.name teacher.family image name description price groups buyCount score topics.time");
+
+        // paginating
+        data = data.facet({
+            data: [{ $skip: (page - 1) * pp }, { $limit: pp }],
+            total: [{ $group: { _id: null, count: { $sum: 1 } } }],
+        });
+
+        // executing query and getting the results
+        const results = await data.exec().catch((e) => {
+            throw e;
+        });
+        const total = results[0].total[0] ? results[0].total[0].count : 0;
+
+        // transform data
+        results[0].data.map((row) => {
+            // TODO : course category
+            let seconds = 0;
+            row.topics.forEach((topic) => {
+                seconds += parseInt(topic.time.hours) * 3600 + parseInt(topic.time.minutes) * 60 + parseInt(topic.time.seconds);
+            });
+            delete row.topics;
+            row.totalTime = new Date(seconds * 1000).toUTCString().match(/(\d\d:\d\d:\d\d)/)[0];
+            return row;
+        });
+
+        return res.json({
+            records: results[0].data,
+            page: page,
+            total: total,
+            pageTotal: Math.ceil(total / pp),
+        });
+    }
 }
