db4f16e655fd31d4c891492fa7ba8951aa806f2e
Kasra Keshvardoost <kasrakeshvardoost@gmail.com>
2022-06-23 10:08:32 +0430

    .

--- src/controllers/admin/importer.controller.ts
+++ src/controllers/admin/importer.controller.ts
@@ -578,13 +578,14 @@ export class ImporterController {
             for (let i = 0; i < json.length; i++) {
                 const row = json[i];
 
-                const user = await this.UserModel.findOne({ email: row.user_email }).exec();
-                if (!user) continue;
+                let user_id = null;
+                const user = await this.UserModel.findOne({ email: row.user_email.toLowerCase() }).exec();
+                if (!!user) user_id = user._id;
 
                 const course = await this.CourseModel.findOne({ oid: row.course_id }).exec();
                 if (!course) continue;
 
-                imports.push({ user: user._id, course: course._id, rating: parseInt(row.rating), createdAt: new Date(row.created_at) });
+                imports.push({ user: user_id, course: course._id, rating: parseInt(row.rating), createdAt: new Date(row.created_at) });
             }
             await this.CourseRatingModel.insertMany(imports);
         } catch (e) {
